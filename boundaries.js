56e8ad71c661839f76b4e7f33bb1eb0a749a4d90
AJ <amarchitelli1993@gmail.com>
2019-09-10 15:09:32 -0400

    commmmmit

---
+++ boundaries.js
@@ -0,0 +1,72 @@
+
+    function checkBoundariesBottom1() {
+      if(gameState.player1.y === config.height) {
+        gameState.player1.y = 1;
+        gameState.player1.y += 1;   
+      } else {
+        gameState.player1.y +=1;
+      }
+    }
+
+    function checkBoundariesTop1() {
+      if(gameState.player1.y === 0) {
+        gameState.player1.y = config.height - 1;
+        gameState.player1.y -= 1;   
+      } else {
+        gameState.player1.y -=1;
+      }
+    }
+
+    function checkBoundariesLeft1() {
+      if(gameState.player1.x === 0) {
+        gameState.player1.x = config.height - 1;
+        gameState.player1.x -= 1;
+      } else {
+        gameState.player1.x -=1;
+      }
+    }
+
+    function checkBoundariesRight1() {
+      if(gameState.player1.x === config.height) {
+        gameState.player1.x = 1 ;
+        gameState.player1.x += 1;
+      } else {
+        gameState.player1.x +=1;
+      }
+    }
+
+    function checkBoundariesBottom2() {
+      if(gameState.player2.y === config.height) {
+        gameState.player2.y =  1;
+        gameState.player2.y += 1;   
+      } else {
+        gameState.player2.y +=1;
+      }
+    }
+
+    function checkBoundariesTop2() {
+      if(gameState.player2.y === 0) {
+        gameState.player2.y = config.height - 1;
+        gameState.movePlayer2.y -= 1;   
+      } else {
+        gameState.player2.y -=1;
+      }
+    }
+
+    function checkBoundariesLeft2() {
+      if(gameState.player2.x === 0) {
+        gameState.player2.x = config.height - 1;
+        gameState.player2.x -= 1;
+      } else {
+        gameState.player2.x -=1;
+      }
+    }
+
+    function checkBoundariesRight2() {
+      if(gameState.player2.x === config.height) {
+        gameState.player2.x = 1 ;
+        gameState.player2.x += 1;
+      } else {
+        gameState.player2.x +=1;
+      }
+    }
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var left2;
 var right1;
 var right2;
 
-//preload--loads in assets--gameState object
+//--------------------------preload--loads in assets--gameState object------------------------------
 
 function preload(){
 
@@ -19,9 +19,10 @@ function preload(){
 }
 
 
-//create--creates game sprites,images,etc
+//-------------------------create--creates game sprites,images,etc--------------------------------
 
 function create(){
+
 
   gameState.player1 = this.add.sprite(30,60,'player1');
   gameState.player2 = this.add.sprite(100,100, 'player2');
@@ -36,7 +37,9 @@ function create(){
     }
   )
   }
-//update--updates constantly,setIn
+
+
+//---------------------------update--updates constantly,setIn-----------------------------------------
 
 function update() {
 
@@ -76,36 +79,34 @@ function update() {
       movePlayer2();
     }
 
-
-
   function movePlayer1() {
     if(gameState.player1.direction === "Down") {
-      down1 = setInterval(function(){ gameState.player1.y +=1 }, 10);
+      down1 = setInterval(function(){ checkBoundariesBottom1() }, 7);
       down1;
     } else if(gameState.player1.direction === "Up") {
-      up1 = setInterval(function(){ gameState.player1.y -=1 }, 10);
+      up1 = setInterval(function(){ checkBoundariesTop1() }, 7);
       up1;
     } else if(gameState.player1.direction === "Left") {
-      left1 = setInterval(function(){ gameState.player1.x -=1 }, 10);
+      left1 = setInterval(function(){ checkBoundariesLeft1() }, 7);
       left1;
     } else if(gameState.player1.direction === "Right") {
-      right1 = setInterval(function(){ gameState.player1.x +=1 }, 10);
+      right1 = setInterval(function(){ checkBoundariesRight1() }, 7);
       right1;
     }
   }
 
   function movePlayer2() {
     if(gameState.player2.direction === "Down") {
-      down2 = setInterval(function(){ gameState.player2.y +=1 }, 10);
+      down2 = setInterval(function(){ checkBoundariesBottom2() }, 7);
       down2;
     } else if(gameState.player2.direction === "Up") {
-      up2 = setInterval(function(){ gameState.player2.y -=1 }, 10);
+      up2 = setInterval(function(){ checkBoundariesTop2() }, 7);
       up2;
     } else if(gameState.player2.direction === "Left") {
-      left2 = setInterval(function(){ gameState.player2.x -=1 }, 10);
+      left2 = setInterval(function(){ checkBoundariesLeft2() }, 7);
       left2;
     } else if(gameState.player2.direction === "Right") {
-      right2 = setInterval(function(){ gameState.player2.x +=1 }, 10);
+      right2 = setInterval(function(){ checkBoundariesRight2() }, 7);
       right2;
     }
   }
@@ -125,17 +126,17 @@ function update() {
     clearInterval(right2);
   }
 }
-//config--object that contains game info
+//---------------------------config--object that contains game info---------------------------------------
 
 const config = {
-  width: 300,
+  width: 500,
   height: 500,
-  backgroundColor: 0xdda0dd,
+  backgroundColor: '#92a8d1',
   scene: {
     preload,
     create,
     update
-  }
+  },
 };
 
 //newPhaser.game
