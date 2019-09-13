7b032003b2318a69c0cba0fe340293f12597cb44
AJ <amarchitelli1993@gmail.com>
2019-09-12 21:42:13 -0400

    random student picture as food

--- index.js
+++ index.js
@@ -1,203 +1,3 @@
-// gameState = {};
-
-// var down1;
-// var down2;
-// var up1;
-// var up2;
-// var left1;
-// var left2;
-// var right1;
-// var right2;
-// var numSnakeSections = 2; //number of snake body sections
-// var snakeSpacer = 16;
-// var snakeSection = new Array(); //array of sprites that make the snake body sections
-// var snakePath = new Array(); 
-
-// //--------------------------preload--loads in assets--gameState object------------------------------
-
-// function preload(){
-
-//   this.load.image('headUp', 'assets/worm_animation/snake_head_up.png')
-//   this.load.image("headDown", 'assets/worm_animation/snake_head_down.png')
-//   this.load.image("headLeft", 'assets/worm_animation/snake_head_left.png')
-//   this.load.image("headRight", 'assets/worm_animation/snake_head_right.png')
-//   this.load.image("tailDown", 'assets/worm_animation/snake_tail_down.png')
-//   this.load.image("tailUp", 'assets/worm_animation/snake_tail_up.png')
-//   this.load.image("tailLeft", 'assets/worm_animation/snake_tail_left.png')
-//   this.load.image("tailRight", 'assets/worm_animation/snake_tail_right.png')
-//   this.load.image("bodyHorizontal", 'assets/worm_animation/snake_body_horizontal.png')
-//   this.load.image("bodyVertical", 'assets/worm_animation/snake_body_vertical.png')
-//   this.load.image("curveBottomLeft", 'assets/worm_animation/snake_curve_bottom_left.png')
-//   this.load.image("curveBottomRight", 'assets/worm_animation/snake_curve_bottom_right.png')
-//   this.load.image("curveTopLeft", 'assets/worm_animation/snake_curve_top_left.png')
-//   this.load.image("curveTopRight", 'assets/worm_animation/snake_curve_top_right.png')
-  
-// }
-
-
-// //-------------------------create--creates game sprites,images,etc--------------------------------
-// //create--creates game sprites,images,etc
-
-// function create(){
-
-//   gameState.player1 = this.add.sprite(30,60, 'headUp');
-//   debugger
-//   gameState.player1.anchor.setTo(0.5, 0.5);
-  
-
-//   for (var i = 1; i <= numSnakeSections - 1; i++) {
-//     snakeSection[i] = game.add.sprite(400, 300, 'bodyVertical');
-//     snakeSection[i].anchor.setTo(0.5, 0.5);
-//   }
-
-//   //  Init snakePath array
-//   for (var i = 0; i <= numSnakeSections * snakeSpacer; i++) {
-//     snakePath[i] = new Phaser.Point(400, 300);
-//   }
-
-
-
-//   gameState.player2 = this.add.sprite(100,100, 'headDown');
-  
-//   gameState.cursors = this.input.keyboard.createCursorKeys();
-//   gameState.wasd = this.input.keyboard.addKeys(
-//     {
-//       up: Phaser.Input.Keyboard.KeyCodes.W,
-//       down: Phaser.Input.Keyboard.KeyCodes.S,
-//       left: Phaser.Input.Keyboard.KeyCodes.A,
-//       right: Phaser.Input.Keyboard.KeyCodes.D,
-//     }
-//   )
-//   }
-
-
-// //---------------------------update--updates constantly,setIn-----------------------------------------
-
-// function update() {
-
-//     if(gameState.cursors.down.isDown) {
-//       gameState.player1.direction = "Down";
-//       stopInterval1();
-//       movePlayer1();
-//     }	else if (gameState.cursors.up.isDown) {
-//       gameState.player1.direction = "Up";
-//       stopInterval1();
-//       movePlayer1();
-//     } else if (gameState.cursors.left.isDown) {
-//       gameState.player1.direction = "Left";
-//       stopInterval1();
-//       movePlayer1();
-//     } else if (gameState.cursors.right.isDown) {
-//       gameState.player1.direction = "Right";
-//       stopInterval1();
-//       movePlayer1();
-//     }
-
-//     if(gameState.wasd.down.isDown) {
-//       gameState.player2.direction = "Down";
-//       stopInterval2();
-//       movePlayer2();
-//     }	else if (gameState.wasd.up.isDown) {
-//       gameState.player2.direction = "Up";
-//       stopInterval2();
-//       movePlayer2();
-//     } else if (gameState.wasd.left.isDown) {
-//       gameState.player2.direction = "Left";
-//       stopInterval2();
-//       movePlayer2();
-//     } else if (gameState.wasd.right.isDown) {
-//       gameState.player2.direction = "Right";
-//       stopInterval2();
-//       movePlayer2();
-//     }
-
-//   function movePlayer1() {
-//     if(gameState.player1.direction === "Down") {
-//       gameState.player1.setTexture('headDown')
-      
-//       down1 = setInterval(function(){ checkBoundariesBottom1() }, 7);
-//       down1;
-//     } else if(gameState.player1.direction === "Up") {
-//       gameState.player1.setTexture('headUp')
-//       up1 = setInterval(function(){ checkBoundariesTop1() }, 7);
-//       up1;
-//     } else if(gameState.player1.direction === "Left") {
-//       gameState.player1.setTexture('headLeft')
-//       left1 = setInterval(function(){ checkBoundariesLeft1() }, 7);
-//       left1;
-//     } else if(gameState.player1.direction === "Right") {
-//       gameState.player1.setTexture('headRight')
-//       right1 = setInterval(function(){ checkBoundariesRight1() }, 7);
-//       right1;
-//     }
-//   }
-
-//   function movePlayer2() {
-//     if(gameState.player2.direction === "Down") {
-
-
-//       var part = snakePath.pop();
-
-//       part.setTo(snakeHead.x, snakeHead.y);
-
-//       snakePath.unshift(part);
-
-//       for (var i = 1; i <= numSnakeSections - 1; i++) {
-//         snakeSection[i].x = (snakePath[i * snakeSpacer]).x;
-//         snakeSection[i].y = (snakePath[i * snakeSpacer]).y;
-//       }
-
-
-
-//       gameState.player2.setTexture('headDown')
-//       down2 = setInterval(function(){ checkBoundariesBottom2() }, 7);
-//       down2;
-//     } else if(gameState.player2.direction === "Up") {
-//       gameState.player2.setTexture('headUp')
-//       up2 = setInterval(function(){ checkBoundariesTop2() }, 7);
-//       up2;
-//     } else if(gameState.player2.direction === "Left") {
-//       gameState.player2.setTexture('headLeft')
-//       left2 = setInterval(function(){ checkBoundariesLeft2() }, 7);
-//       left2;
-//     } else if(gameState.player2.direction === "Right") {
-//       gameState.player2.setTexture('headRight')
-//       right2 = setInterval(function(){ checkBoundariesRight2() }, 7);
-//       right2;
-//     }
-//   }
-
-
-//   function stopInterval1() {
-//     clearInterval(down1);
-//     clearInterval(up1);
-//     clearInterval(left1);
-//     clearInterval(right1);
-//   }
-
-//   function stopInterval2() {
-//     clearInterval(down2);
-//     clearInterval(up2);
-//     clearInterval(left2);
-//     clearInterval(right2);
-//   }
-// }
-// //---------------------------config--object that contains game info---------------------------------------
-
-// const config = {
-//   width: 1000,
-//   height: 1000,
-//   backgroundColor: 0xdda0dd,
-//   scene: {
-//     preload,
-//     create,
-//     update
-//   },
-// };
-
-// //newPhaser.game
-// const game = new Phaser.Game(config);
-
 var config = {
   type: Phaser.WEBGL,
   width: 640,
@@ -226,6 +26,8 @@ var snake_one_score_counter;
 var text_snake_one;
 var text_snake_two;
 var text2;
+var students;
+var item;
 
 //  Direction consts
 var UP = 0;
@@ -238,25 +40,18 @@ var game = new Phaser.Game(config);
 function preload() {
   this.load.image('food', 'assets/games/snake/rabbit.png');
   this.load.image('tony', 'assets/games/snake/tony.png');
-  this.load.image('headUp', 'assets/worm_animation/snake_head_up.png')
-  this.load.image("headDown", 'assets/worm_animation/snake_head_down.png')
-  this.load.image("headLeft", 'assets/worm_animation/snake_head_left.png')
-  this.load.image("headRight", 'assets/worm_animation/snake_head_right.png')
-  this.load.image("tailDown", 'assets/worm_animation/snake_tail_down.png')
-  this.load.image("tailUp", 'assets/worm_animation/snake_tail_up.png')
-  this.load.image("tailLeft", 'assets/worm_animation/snake_tail_left.png')
-  this.load.image("tailRight", 'assets/worm_animation/snake_tail_right.png')
-  this.load.image("bodyHorizontal", 'assets/worm_animation/snake_body_horizontal.png')
-  this.load.image("bodyVertical", 'assets/worm_animation/snake_body_vertical.png')
-  this.load.image("curveBottomLeft", 'assets/worm_animation/snake_curve_bottom_left.png')
-  this.load.image("curveBottomRight", 'assets/worm_animation/snake_curve_bottom_right.png')
-  this.load.image("curveTopLeft", 'assets/worm_animation/snake_curve_top_left.png')
-  this.load.image("curveTopRight", 'assets/worm_animation/snake_curve_top_right.png')
-  this.load.image("curveTopRight", 'assets/ground_tiles/grass.jpg')
   this.load.image('coffee', 'assets/games/snake/coffee.png');
   this.load.image('flatiron', 'assets/games/snake/flatiron.png');
   this.load.image('greg', 'assets/games/snake/greg.png');
   this.load.image('questions', 'assets/games/snake/questions.png');
+  this.load.image('aj', 'assets/games/students/aj.png');
+  this.load.image('meeghann', 'assets/games/students/meeghann.png');
+  this.load.image('varun', 'assets/games/students/varun.png');
+  this.load.image('jason', 'assets/games/students/jason.png');
+  this.load.image('hyo', 'assets/games/students/hyo.png');
+  this.load.image('alyssa', 'assets/games/students/alyssa.png');
+  this.load.image('val', 'assets/games/students/val.png');
+  this.load.image('andrew', 'assets/games/students/andrew.png');
 }
 
 function create() {
@@ -269,7 +64,7 @@ function create() {
       function Food(scene, x, y) {
         Phaser.GameObjects.Image.call(this, scene)
 
-        this.setTexture('questions');
+        this.setTexture('aj');
         this.setPosition(x * 16, y * 16);
         this.setOrigin(0);
 
@@ -309,8 +104,8 @@ function create() {
         
       });
     
-    
-    snakes = []
+    students = ['aj','varun','meeghann', 'andrew', 'val', 'alyssa', 'hyo', 'jason'];
+    snakes = [];
   var Snake = new Phaser.Class({
 
     initialize:
@@ -626,6 +421,8 @@ function repositionFood() {
 
     //  And place it
     food.setPosition(pos.x * 16, pos.y * 16);
+    item = students.sort(function() {return 0.5 - Math.random()})[0];
+    food.setTexture(item)
 
     return true;
   }
@@ -707,7 +504,8 @@ function repositionFood() {
 
     //  And place it
     food.setPosition(pos.x * 16, pos.y * 16);
-
+    item = students.sort(function() {return 0.5 - Math.random()})[0];
+    food.setTexture(item)
     return true;
   }
   else {
